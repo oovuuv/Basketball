ce91f84143f59cbd4c82d844a3cf42feaa2fe797
vooa <39353261+oovuuv@users.noreply.github.com>
2018-07-17 15:45:23 +0900

    컴퓨터, 사용자 점수 올리기 완료.

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,57 @@
 
 var comScore = 0;
+var userScore = 0;
 
 function onComputerShoot() {
 
   var textElem = document.getElementById('text');
-
+  var comScoreElem = document.getElementById('computer-score');
   var shootType = Math.random() < 0.5 ? 2 : 3;
     // Math.random 값이 0.5보다 작다면 '2', 크다면 '3'을 shootType 변수에 대입하라.라는 '삼향연산자'이다.
 
-
   if (shootType === 2) {
     if(Math.random() < 0.5) {
-      textElem.innerHTML = '컴퓨터가 2점슛을 성공시켰습니다!';
+      textElem.innerHTML = '컴퓨터가 2점 슛을 성공시켰습니다!';
 
       comScore += 2;
-
-      var comScoreElem = document.getElementById('computer-score');
       comScoreElem.innerHTML = comScore;
     } else {
-      textElem.innerHTML = '컴퓨터가 2점슛을 실패했습니다.';
+      textElem.innerHTML = '컴퓨터가 2점 슛을 실패했습니다.';
     }
   } else {
     if (Math.random() < 0.33) {
-      textElem.innerHTML = '컴퓨터가 3점슛을 성공시켰습니다!';
+      textElem.innerHTML = '컴퓨터가 3점 슛을 성공시켰습니다!';
 
       comScore += 3;
-
-      var comScoreElem = document.getElementById('computer-score');
       comScoreElem.innerHTML = comScore;
     } else {
-      textElem.innerHTML = '컴퓨터가 3점슛을 실패했습니다.';
+      textElem.innerHTML = '컴퓨터가 3점 슛을 실패했습니다.';
+    }
+  }
+}
+
+function onUserShoot(shootType) {
+
+  var textElem = document.getElementById('text');
+  var userScoreElem = document.getElementById('user-score');
+
+  if (shootType === 2) {
+    if(Math.random() < 0.5) {
+      textElem.innerHTML = '2점 슛 성공!!';
+
+      userScore += 2;
+      userScoreElem.innerHTML = userScore;
+    } else {
+      textElem.innerHTML = '2점 슛을 실패했습니다.'
+    }
+  } else {
+    if (Math.random() < 0.33) {
+      textElem.innerHTML = '3점 슛 성공!!';
+
+      userScore += 3;
+      userScoreElem.innerHTML = userScore;
+    } else {
+      textElem.innerHTML = '3점 슛을 실패했습니다.'
     }
   }
 }
